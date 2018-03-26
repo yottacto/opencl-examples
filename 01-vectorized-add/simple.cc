d8b43f620835d50972a9f5e144ec2306a9dfb8fb
yottacto <yotta.w.cto@gmail.com>
2018-03-26 23:40:44 +0800

    [+] naive for loop vec add

---
+++ 01-vectorized-add/simple.cc
@@ -0,0 +1,24 @@
+// ml:run = time -p $bin > output
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+using value_type = double;
+auto constexpr size = 10'000'000u;
+
+int main()
+{
+    std::vector<value_type> a(size);
+    std::vector<value_type> b(size);
+    std::vector<value_type> c(size);
+    std::iota(std::begin(a), std::end(a), 0);
+    std::iota(std::begin(b), std::end(b), 0);
+
+    for (auto i = 0u; i < size; i++)
+        c[i] = a[i] + b[i];
+
+    for (auto i = 0u; i < size; i++)
+        std::cout << c[i] << " ";
+    std::cout << "\n";
+}
+
