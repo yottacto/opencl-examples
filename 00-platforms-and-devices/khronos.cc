e7480d99d51b0b5ad46f3745f56f1e2cf47e8a2e
yottacto <yotta.w.cto@gmail.com>
2018-03-27 08:43:46 +0800

    [@] remove redundant std::left (sticky)

--- 00-platforms-and-devices/khronos.cc
+++ 00-platforms-and-devices/khronos.cc
@@ -9,15 +9,13 @@ template <class T>
 std::string memory_GB(T const& byte)
 {
     std::string size;
+    std::string size_GB;
     {
         std::stringstream buf;
         buf << byte;
         buf >> size;
-    }
+        buf.clear();
 
-    std::string size_GB;
-    {
-        std::stringstream buf;
         double dsize = std::stod(size) / (1<<30);
         buf << std::fixed << std::setprecision(2) << dsize;
         buf >> size_GB;
@@ -37,20 +35,14 @@ int main()
         std::cout << "  " << std::left << std::setw(width) << "Platform Name" << p.getInfo<CL_PLATFORM_NAME>() << "\n";
 
         std::vector<cl::Device> devices;
-
-        // CL_DEVICE_TYPE_DEFAULT
-        // CL_DEVICE_TYPE_CPU
-        // CL_DEVICE_TYPE_GPU
-        // CL_DEVICE_TYPE_ACCELERATOR
-        // CL_DEVICE_TYPE_ALL
-        p.getDevices(CL_DEVICE_TYPE_DEFAULT, &devices);
+        p.getDevices(CL_DEVICE_TYPE_ALL, &devices);
 
         for (auto const& d : devices) {
-            std::cout << std::left << std::setw(width) << "Device Name"         << d.getInfo<CL_DEVICE_NAME>()                       << "\n";
-            std::cout << std::left << std::setw(width) << "Device Version"      << d.getInfo<CL_DEVICE_VERSION>()                    << "\n";
-            std::cout << std::left << std::setw(width) << "Max compute units"   << d.getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>()          << "\n";
-            std::cout << std::left << std::setw(width) << "Max clock frequency" << d.getInfo<CL_DEVICE_MAX_CLOCK_FREQUENCY>()        << "MHz\n";
-            std::cout << std::left << std::setw(width) << "Global memory size"  << memory_GB(d.getInfo<CL_DEVICE_GLOBAL_MEM_SIZE>()) << "\n";
+            std::cout << std::setw(width) << "Device Name"         << d.getInfo<CL_DEVICE_NAME>()                       << "\n";
+            std::cout << std::setw(width) << "Device Version"      << d.getInfo<CL_DEVICE_VERSION>()                    << "\n";
+            std::cout << std::setw(width) << "Max compute units"   << d.getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>()          << "\n";
+            std::cout << std::setw(width) << "Max clock frequency" << d.getInfo<CL_DEVICE_MAX_CLOCK_FREQUENCY>()        << "MHz\n";
+            std::cout << std::setw(width) << "Global memory size"  << memory_GB(d.getInfo<CL_DEVICE_GLOBAL_MEM_SIZE>()) << "\n";
         }
     }
 
